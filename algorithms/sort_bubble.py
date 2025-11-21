1cd8fa4c6bc6ce3deb84a0ede4f0c45feaf9b54a
rominaale-oss <rominaalejandraescalada@gmail.com>
2025-11-21 17:29:44 -0300

    agregue comentarios

--- algorithms/sort_bubble.py
+++ algorithms/sort_bubble.py
@@ -1,20 +1,20 @@
 # Contrato: init(vals), step() -> {"a": int, "b": int, "swap": bool, "done": bool}
 
-items = []
-n = 0
+items = [] #lista a ordenar
+n = 0      #cantidad de items
 i = 0
 j = 0
 
 def init(vals):
     global items, n, i, j
     items = list(vals)
-    n = len(items)
+    n = len(items)  
     i = 0
     j = 0
 def step():
     global items, n, i, j
 
-    if n<=1 or i>=n-1:
+    if n<=1 or i>=n-1:    #si no hay nada que ordenar o termino todas las pasadas
         return {"done": True}
 
     a=j
@@ -25,15 +25,15 @@ def step():
         items[a], items[b] = items[b], items[a]
         swap=True
 
-    j += 1
+    j =j+1  #avanza la "mini pasada"
 
     if j>=n-i-1:
-        j=0
-        i=i+1
+        j=0    #reiniciamos las "mini pasadas"
+        i=i+1  #incrementamos el contador de pasadas
 
     if i>=n-1:
-        return {"done": True}
-
+        return {"done": True}   
+#se devuelve la info del visualizador
     return {"a": a, "b": b, "swap": swap, "done": False}
 
  
