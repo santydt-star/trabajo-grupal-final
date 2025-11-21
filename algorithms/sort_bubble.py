6305b6ac5c18bdfcb0d3e4a1b77a0555d493cc03
rominaale-oss <rominaalejandraescalada@gmail.com>
2025-11-21 17:13:10 -0300

    nuevo step

--- algorithms/sort_bubble.py
+++ algorithms/sort_bubble.py
@@ -12,17 +12,28 @@ def init(vals):
     i = 0
     j = 0
 def step():
-    for i in range(n):
-        for j in range(n):
-            if(items[i]<items[j]):
-                items[i], items[j] = items[j], items[i]
-                swap=True
-                return {"i": i, "j": j, "swap": True, "done": False}
-    # TODO:
-    # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
-    # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
-    # 3) Avanzar punteros (preparar el próximo paso).
-    # 4) Devolver {"a": a, "b": b, "swap": swap, "done": False}.
-    #
-    # Cuando no queden pasos, devolvé {"done": True}.
-    return {"done": True}
+    global items, n, i, j
+
+    if n<=1 or i>=n-1:
+        return {"done": True}
+
+    a=j
+    b=j+1
+    swap=False
+
+    if items[a] > items[b]:
+        items[a], items[b] = items[b], items[a]
+        swap=True
+
+    j += 1
+
+    if j>=n-i-1:
+        j=0
+        i=i+1
+
+    if i>=n-1:
+        return {"done": True}
+
+    return {"a": a, "b": b, "swap": swap, "done": False}
+
+ 
