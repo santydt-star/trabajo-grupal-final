a2d34b459b4be7bf72c63a3d6cf15c7fe4a3c132
santydt-star <santytaboada06@gmail.com>
2025-11-24 22:14:16 -0300

    segundo intento de quick

--- sort_quick.py
+++ sort_quick.py
@@ -3,21 +3,28 @@
 
 items = []
 n = 0
+i=0
+j=0
+pivote=0
+fase=""
 # Agregá acá tus punteros/estado, p.ej.:
 # i = 0; j = 0; fase = "x"; stack = []
 
 def init(vals):
-    global items, n, i, j, pivote
+    global items, n, i, j, pivote, fase
     items = list(vals)
     n = len(items)
     i=-1
     j=0
     pivote=n-1
+    fase= "particion"
     # TODO: inicializar punteros/estado
 
 def step():
-    global items, n, i, j, pivote
-    if pivote>=0:
+    global items, n, i, j, pivote, fase
+    if pivote<=0:
+        return {"done": True}
+    if fase=="particion":
         if j<pivote:
             if items[j]<items[pivote]:
                 i=i+1
@@ -30,14 +37,15 @@ def step():
         if j==pivote:
            i=i+1
            items[i],items[pivote]=items[pivote],items[i]
-           return {"a": i, "b": pivote, "swap": True, "done": False}
-        if j>pivote:
-            pivote=pivote-1
-            i=-1
-            j=0
-            return {"a": None, "b":None, "swap":False, "done": False}
-    if pivote<0:
-        return {"done": True}
+           fase="reset"
+           return {"a": i, "b": pivote, "swap": True, "done": False} 
+    if fase=="reset":
+        i=-1
+        pivote=pivote-1
+        j=0
+        fase="particion"
+        return {"a": j, "b":pivote,"swap":False, "done": False}
+    return {"done": True}
     # TODO: implementar UN micro-paso de tu algoritmo y devolver el dict.
     # Recordá:
     # - a, b dentro de [0, n-1]
